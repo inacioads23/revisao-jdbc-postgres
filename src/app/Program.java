bfea8c678fa460e831d2a5d7f1ccb6282dfa3df0
inacioads23 <jinacio11@gmail.com>
2025-01-28 13:04:10 -0300

    Cloned project and Update files db.properties, Program.java

--- src/app/Program.java
+++ src/app/Program.java
@@ -11,12 +11,16 @@ public class Program {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection conn = DB.getConnection();
-	
-		Statement st = conn.createStatement();
-			
-		ResultSet rs = st.executeQuery("select * from tb_product");
-			
+		Connection conn = DB.getConnection(); //faz conexão com o DB	
+		
+		//instancia/ cria um objeto do tipo Statement, que é o comando SQL para o DB
+		//uso o comando "st" do tipo Statement para executar a Query
+		Statement st = conn.createStatement(); 
+		
+		//armazena os dados da consulta
+		ResultSet rs = st.executeQuery("select * from tb_product"); 
+		
+		//enquanto houver dado ele mostra
 		while (rs.next()) {
 			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
 		}
